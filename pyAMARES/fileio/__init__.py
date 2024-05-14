5fe17bd4b71b33bf7b8aa713da4efbb3c1eab42e
Jia Xu <jia-xu-1@uiowa.edu>
2024-05-13 21:13:46 -0500

    add read_nifti to file I/O

--- pyAMARES/fileio/__init__.py
+++ pyAMARES/fileio/__init__.py
@@ -1,1 +1,2 @@
 from .readmat import readmrs
+from .readmat import read_nifti
--- pyAMARES/fileio/readmat.py
+++ pyAMARES/fileio/readmat.py
@@ -6,7 +6,8 @@ def readmrs(filename):
     """
     Reads MRS data from a file, supporting multiple file formats including ASCII, CSV, NPY, and MATLAB files.
 
-    This function detects the file format based on the file extension and loads the MRS data accordingly. For ASCII files, it expects two columns representing the real and imaginary parts. NPY files should contain a NumPy array, and MATLAB files should contain a variable named 'fid'.
+    This function detects the file format based on the file extension and loads the MRS data accordingly. 
+    For ASCII files, it expects two columns representing the real and imaginary parts. NPY files should contain a NumPy array, and MATLAB files should contain a variable named 'fid'.
 
     Args:
         filename (str): The path and name of the file from which to load the MRS data.
@@ -54,3 +55,60 @@ def readmrs(filename):
     print("data.shape=", data.shape)
     assert len(data.shape) == 1
     return data
+
+def read_nifti(filename):
+    """
+    Reads MRS data from a NIfTI-MRS file, it assumes single voxel spectroscopy (SVS), and returns a header and an 1D FID array
+
+    Args:
+        filename (str): The path and name of the NIfTI file to load.
+
+    Returns:
+        tuple: A tuple containing:
+            - header (argparse.Namespace): A namespace object with center frequency (MHz), spectral width (sw), dwell time (second), and optionally dead time (second).
+            - fid (numpy.ndarray): A complex numpy array containing the frequency-domain MRS data.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        IndexError: If the required header extension is not found in the NIfTI file.
+        KeyError: If essential metadata is missing in the NIfTI header.
+        ImportError: If nibabel is not installed.
+
+    Example:
+        >>> header, fid = read_nifti('example_mrs.nii')
+        >>> print(header.MHz, header.sw)
+        >>> print(fid.shape)
+
+    Note:
+        - This function is a wrapper of the minimal example of loading Nifti-MRS using nibabel:
+          https://github.com/wtclarke/nifti_mrs_python_example/tree/86a305f28a45f0d07aab29f52daf3a5d880438d8
+        - The 'AcquisitionStartTime' is optionally loaded into the header as 'deadtime'. If it is absent, a message is printed but no exception is thrown.
+        - Errors related to missing nibabel or JSON handling are not caught explicitly but will result in standard Python exceptions being raised.
+    """
+    # This is a wrapper of the minimal exampe of loading Nifti-MRS using nibabel
+    # https://github.com/wtclarke/nifti_mrs_python_example/tree/86a305f28a45f0d07aab29f52daf3a5d880438d8
+    import json
+    import nibabel as nib  # should be installed together with spec2nii
+    import argparse
+
+    img = nib.load(filename)
+    data = img.get_fdata(dtype=np.complex64)
+    fid = data.squeeze() # Assume SVS
+    hdr_ext_codes = img.header.extensions.get_codes()
+    mrs_hdr_ext = json.loads(img.header.extensions[hdr_ext_codes.index(44)].get_content())
+    MHz = mrs_hdr_ext['SpectrometerFrequency'][0]
+    sw = mrs_hdr_ext['SpectralWidth']
+    dwelltime = 1 / sw
+    header = argparse.Namespace()
+    header.MHz = MHz
+    header.dwelltime = dwelltime
+    header.sw = sw
+    try:
+        mrs_hdr_ext['AcqusitionStartTime']
+        header.deadtime = mrs_hdr_ext['AcqusitionStartTime']
+    except:
+        print("There is no AcqusitionStartTime!")
+    return header, fid
+
+
+
