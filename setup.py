3260a2859dfc2b32b2e6bff68b4c3de2873434c3
Jia Xu <jia-xu-1@uiowa.edu>
2025-03-23 20:57:23 -0500

    add tests as jupyter notebooks

--- setup.py
+++ setup.py
@@ -84,6 +84,8 @@ install_requires=[
     "mat73",
     "ipython",
     "ipykernel",
+    "ipywidgets>=7.6.0,<8.0.0;python_version<'3.11'",  # For older Python versions
+    "ipywidgets>=8.0.0;python_version>='3.11'",        # For newer Python versions
 ]
 
 # Use the better-performing 'hlsvdpro' package if running on supported platforms
