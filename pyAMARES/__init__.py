5eb023fe58834fb378ccf54a62c84776a11e3a27
Jia Xu <jia-xu-1@uiowa.edu>
2025-04-12 14:22:43 -0500

    circumvent F403 for rff check

--- pyAMARES/__init__.py
+++ pyAMARES/__init__.py
@@ -4,7 +4,19 @@ __version__ = "0.3.26dev"
 # print("Current pyAMARES version is %s" % __version__)
 # print("Author: %s" % __author__)
 
-from .kernel import *
-from .util import *
-from .libs import *
-from .fileio import *
+from .kernel import *  # noqa: F403
+from .util import *  # noqa: F403
+from .libs import *  # noqa: F403
+from .fileio import *  # noqa: F403
+
+__all__ = []
+
+from .kernel import __all__ as kernel_all
+from .util import __all__ as util_all
+from .libs import __all__ as libs_all
+from .fileio import __all__ as fileio_all
+
+__all__.extend(kernel_all)
+__all__.extend(util_all)
+__all__.extend(libs_all)
+__all__.extend(fileio_all)
